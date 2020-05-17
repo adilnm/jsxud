70bc6925677db72aa63e7279a2491137ec0fb4d4
Adil Naimi <adilnm23@gmail.com>
2020-05-17 03:04:52 -0500

    create a class components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import Comments from "./components/Comments";
-import ApprovalCard from "./components/ApprovalCard";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <Comments name="Sam" timeAgo="Today at 6:00pm" content="Great" />
-      </ApprovalCard>
-      <ApprovalCard>
-        <Comments name="Adil" timeAgo="Yesterday at 3:00pm" content="Not bad" />
-      </ApprovalCard>
-    </div>
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
   );
+  return <div className="">Latitude: </div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
