e808ada7a5beb90866f1d60edb82a5e8e949daaf
Adil Naimi <adilnm23@gmail.com>
2020-05-17 23:22:24 -0500

    Determin which season based on month and Latitude

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export default function SeasonDisplay() {
-    return (
-        <div>
-            Season Display
-        </div>
-    )
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
+export default function SeasonDisplay(props) {
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season)
+  return <div>Season Display</div>;
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div className="">Latitude: </div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
+
+  }
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState({ lat: position.coords.latitude });
+        },
+        err => {
+          this.setState({ errorMessage: err.message });
+        });
+  }
+ 
+  render() {
+    if(!this.state.lat&&this.state.errorMessage){
+        return <div>Error: {this.state.errorMessage}</div>
+    }
+    else if(this.state.lat&&!this.state.errorMessage){
+        return <SeasonDisplay lat={this.state.lat}/>
+    }
+    return <div>Loading...!</div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
